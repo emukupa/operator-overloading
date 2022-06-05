211b2e1ea8bf11c2b6eedd8a3beb54a3e5864d84
emukupa1 <edward-manda@uhatok.com>
2022-06-05 01:27:04 -0500

    use the class printing raw string

--- main.cpp
+++ main.cpp
@@ -1,8 +1,41 @@
-#include <cstdio>
+#include <iostream>
 #include "Rational.h"
+
+std::ostream &operator<<(std::ostream &o, const Rational &r)
+{
+    return o << r.raw_string();
+}
 
 int main()
 {
-    puts("Hello World");
+    Rational a = 7; // 7/1
+    std::cout << "a is: " << a << std::endl;
+
+    Rational b(25, 15); // 5/3
+    std::cout << "b is: " << b << std::endl;
+
+    Rational c = b; // copy constructor
+    std::cout << "c is: " << c << std::endl;
+
+    Rational d; // default constructor
+    std::cout << "d is: " << d << std::endl;
+
+    d = c; // assignment
+    std::cout << "d is: " << d << std::endl;
+
+    Rational &e = d; // reference
+    d = e;           // assignment to self!
+    std::cout << "e is: " << e << std::endl;
+
+    std::cout << std::endl;
+    std::cout << "d is: " << d.string() << std::endl;
+
+    std::cout << std::endl;
+    std::cout << a << " + " << b << " = " << a + b << std::endl;
+    std::cout << a << " - " << b << " = " << a - b << std::endl;
+    std::cout << a << " * " << b << " = " << a * b << std::endl;
+    std::cout << a << " / " << b << " = " << a / b << std::endl;
+    // std::cout << a << " + " << b << " = " << a + b << std::endl;
+
     return 0;
 }
